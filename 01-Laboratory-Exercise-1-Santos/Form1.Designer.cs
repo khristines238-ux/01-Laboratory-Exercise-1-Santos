718c3034bfa87a798ed079ddbb644ccafe0b42ef
khristines238 <khristines238@gmail.com>
2025-08-14 19:58:05 +0800

    nag add ng label for registration form

--- 01-Laboratory-Exercise-1-Santos/Form1.Designer.cs
+++ 01-Laboratory-Exercise-1-Santos/Form1.Designer.cs
@@ -34,17 +34,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 54);
+            this.label1.Font = new System.Drawing.Font("Elephant", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(101, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(285, 35);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Registration Form";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(497, 450);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
